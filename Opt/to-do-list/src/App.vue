63ca063c223b3a8acabcdfe34b732b0a60513a66
lightxlamp <sheveliukhin.s@gmail.com>
2020-09-20 13:10:35 +0600

    init project #2

---
+++ Opt/to-do-list/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" class="logo" src="./assets/_logo.png">
+    <HelloWorld msg="Welcome to TODO App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  width: 200px;
+  color: red;
+}
+
+</style>
