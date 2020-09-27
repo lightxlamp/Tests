72600a3cdc0619a13c03b07400aec4a00ec0a38d
lightxlamp <sheveliukhin.s@gmail.com>
2020-09-27 20:29:18 +0600

    style: add some basic design to posts

--- FS/posts-and-users/src/components/PostsList.vue
+++ FS/posts-and-users/src/components/PostsList.vue
@@ -1,15 +1,18 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <v-row justify="center"  
+    <v-row>
+      <v-col>
+        <v-row  
           v-for="(post, i) in postsToDisplay" 
           :key="i"      
           class="subheading mx-3">
-          {{ post }}
+          <div class="post">
+            <div class="post__header">
+              <div class="post__id"> {{ post.id }} </div>
+              <div class="post__title">{{ post.title }}</div>
+            </div>
+            <div class="post__body">{{ post.body }}</div>
+          </div>
         </v-row>
       </v-col>
     </v-row>
@@ -32,3 +35,29 @@
     } 
   }
 </script>
+
+<style lang="scss" scoped>
+  .post {
+    text-align: left;
+    margin-bottom: 2rem;
+    border: .3rem solid #4b3c39;
+    padding: 1rem;
+    border-radius: 1rem;
+    width: 100%;
+
+    &__header {
+      display: flex;
+      flex-direction: row;
+      font-size: 1.5rem;
+      text-transform: uppercase;
+    }
+
+    &__body {
+      font-size: 1.1rem;
+    }
+
+    &__id {
+      margin-right: 1rem;
+    }
+  }
+</style>
